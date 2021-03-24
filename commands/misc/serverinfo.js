16e395b28bb0789e57a2d8cb81c5875a50963cd6
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2021-03-24 09:58:07 -0400

    Update serverinfo.js

--- commands/misc/serverinfo.js
+++ commands/misc/serverinfo.js
@@ -16,12 +16,12 @@ module.exports = class ServerInfoCommand extends Command {
             .sort((a, b) => b.position - a.position)
             .map(r => r)
             .join("");
-            if (rolemap.length > 1024) rolemap = "Sorry but there is too many roles to display";
+            if (rolemap.length > 1024) rolemap = "Sorry but there is too many roles to display.";
             if (!rolemap) rolemap = "Error: no roles found";
        const Embed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('Server Info')
-	.setAuthor(`Server Acromyn- ${message.guild.nameAcronym}`)
+	.setAuthor(`Server Initials- ${message.guild.nameAcronym}`)
 	.setDescription(`${message.guild.name}`)
 	.setThumbnail(`${message.guild.iconURL({ format: "png", dynamic: true })}`)
     .addFields(
